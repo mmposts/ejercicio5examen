e21b0976a90ff8c8f49b530a02699c91b22e30c0
Maya <fox>
2023-05-30 11:45:41 +0200

    Diccionario de una lista de alumnos

---
+++ diccionario_alumnos.py
@@ -0,0 +1,17 @@
+alumnos = {
+    "alumno1" :{
+        "name" : "Juan Perez",
+        "age" :18,
+        "curso" : 12
+    },
+    "alumno2" :{
+        "name":"Maria Lopez",
+        "age" : 17,
+        "curso":11  
+    },
+    "alumno3" :{
+        "name": "Pedro Gomez",
+        "age" :19,
+        "curso":12
+    }
+}
